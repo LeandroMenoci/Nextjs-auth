66c3b4395e50bedbf7c4964837238c1ab10fa777
leandromenoci <menoci.leandro@gmail.com>
2022-08-24 23:58:50 -0300

    Projeto em Nextjs curso alura, criado serviço para gerar o token e salvar nos cookies

---
+++ frontend/pages/auth-page-ssr.jsx
@@ -0,0 +1,28 @@
+import { tokenService } from "../src/services/auth/tokenService"
+import nookies from 'nookies'
+
+export default function AuthPageSSR(props) {
+
+  return (
+    <div>
+      <h1>
+        Auth Page SSR
+      </h1>
+      <pre>
+        {JSON.stringify(props, null, 2)}
+      </pre>
+    </div>
+  )
+}
+
+export async function getServerSideProps(ctx) {
+  console.log(tokenService.get())
+  const cookies = nookies.get(ctx)
+  console.log('cookies', cookies)
+
+  return {
+    props: {
+      token: tokenService.get(ctx)
+    }
+  }
+}
